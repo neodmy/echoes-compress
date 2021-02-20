0f1cc70e7ffd6238915b24529cd21813c75d5d87
David Yusta <david.yusta@guidesmiths.com>
2021-02-20 20:10:52 +0100

    refactor: remove compressor controller

--- components/compressor/index.js
+++
@@ -1,6 +0,0 @@
-const System = require('systemic');
-const initCompressor = require('./initCompressor');
-
-module.exports = new System({ name: 'compressor' })
-  .add('compressor', initCompressor())
-  .dependsOn('config', 'logger', 'archiver');
